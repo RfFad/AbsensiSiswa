7c3951d8f503dfc26513729545caab6a9799374b
RfFad <ashiap135@gmail.com>
2025-03-27 11:26:42 +0700

    db

--- src/configs/Databases.js
+++ src/configs/Databases.js
@@ -4,7 +4,9 @@ const connect = mysql.createConnection({
     host: "bydhxjrth2bmh8ybxfem-mysql.services.clever-cloud.com",
     user: "ud70zixcu63ynzst",
     password: "qywqGf9V5E2SWl1PtAdQ",
-    database: "bydhxjrth2bmh8ybxfem"
+    database: "bydhxjrth2bmh8ybxfem",
+    port: 3306, 
+    ssl: { rejectUnauthorized: false }
 });
 
 module.exports = connect;
