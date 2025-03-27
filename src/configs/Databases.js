b04470835b483d48dd8cd5123d819e66b662bdf6
RfFad <ashiap135@gmail.com>
2025-03-27 10:50:23 +0700

    Database Setting

--- src/configs/Databases.js
+++ src/configs/Databases.js
@@ -1,11 +1,11 @@
+require('dotenv').config();
 const mysql = require('mysql');
 
 const connect = mysql.createConnection({
-    host: "bydhxjrth2bmh8ybxfem-mysql.services.clever-cloud.com",
-    user: "ud70zixcu63ynzst",
-    password: "qywqGf9V5E2SWl1PtAdQ",
-    database: "bydhxjrth2bmh8ybxfem"
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASS,
+    database: process.env.DB_NAME
 });
-
 
 module.exports = connect;
