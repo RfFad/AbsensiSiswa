14c9c95445e15b25966a6cd4444d67d19caa730f
RfFad <ashiap135@gmail.com>
2025-03-27 11:14:06 +0700

    database service

--- src/configs/Databases.js
+++ src/configs/Databases.js
@@ -1,10 +1,10 @@
 const mysql = require('mysql');
 
 const connect = mysql.createConnection({
-    host: process.env.DB_HOST,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASS,
-    database: process.env.DB_NAME
+    host: "bydhxjrth2bmh8ybxfem-mysql.services.clever-cloud.com",
+    user: "ud70zixcu63ynzst",
+    password: "qywqGf9V5E2SWl1PtAdQ",
+    database: "bydhxjrth2bmh8ybxfem"
 });
 
 module.exports = connect;
