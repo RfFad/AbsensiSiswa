4fda4112833ef1eac47463fa5bb4f14e89a8c2a2
RfFad <ashiap135@gmail.com>
2025-03-27 10:20:24 +0700

    database server

--- src/configs/Databases.js
+++ src/configs/Databases.js
@@ -1,10 +1,10 @@
 const mysql = require('mysql');
 
 const connect = mysql.createConnection({
-    host: "localhost",
-    user: "root",
-    password: "",
-    database: "absensi_app"
+    host: "bydhxjrth2bmh8ybxfem-mysql.services.clever-cloud.com",
+    user: "ud70zixcu63ynzst",
+    password: "qywqGf9V5E2SWl1PtAdQ",
+    database: "bydhxjrth2bmh8ybxfem"
 });
 
 
